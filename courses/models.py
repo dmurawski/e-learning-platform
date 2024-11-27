bc85b099128f3eb280a467e4e9c009c5c966afaa
dmurawski <dmurawski018@gmail.com>
2024-11-27 23:28:11 +0100

    add models and create database

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,49 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Subject(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ("title",)
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Course(models.Model):
+    owner = models.ForeignKey(
+        User,
+        related_name="courses_created",
+        on_delete=models.CASCADE,
+    )
+    subject = models.ForeignKey(
+        Subject,
+        on_delete=models.CASCADE,
+        related_name="courses",
+    )
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+    overview = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ("-created",)
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Module(models.Model):
+    course = models.ForeignKey(
+        Course,
+        related_name="modules",
+        on_delete=models.CASCADE,
+    )
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+
+    def __str__(self) -> str:
+        return self.title
