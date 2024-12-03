acf98f96a79e5add83cc619a513bca4c41999f94
dmurawski <dmurawski018@gmail.com>
2024-12-03 13:06:20 +0100

    add chat room

--- educa/urls.py
+++ educa/urls.py
@@ -29,6 +29,7 @@ urlpatterns = [
     path("course/", include("courses.urls")),
     path("", CourseListView.as_view(), name="course_list"),
     path("students/", include("students.urls")),
+    path("chat/", include("chat.urls", namespace="chat")),
     path("api/", include("courses.api.urls", namespace="api")),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
