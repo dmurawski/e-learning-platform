58021d656f88409bd883676752e782248022957a
dmurawski <dmurawski018@gmail.com>
2024-12-03 18:22:36 +0100

    add docker

--- educa/settings/prod.py
+++ educa/settings/prod.py
@@ -1,14 +1,26 @@
 from .base import *
+from decouple import config
 
 DEBUG = False
 
 ADMINS = [
     ("admin admin", "admin@email.com"),
 ]
+
 ALLOWED_HOSTS = ["*"]
+
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("POSTGRES_DB"),
+        "USER": config("POSTGRES_USER"),
+        "PASSWORD": config("POSTGRES_PASSWORD"),
+        "HOST": "db",
+        "PORT": 5432,
     }
 }
+
+REDIS_URL = "redis://cache:6379"
+CACHES["default"]["LOCATION"] = REDIS_URL
+CHANNEL_LAYERS["default"]["CONFIG"]["hosts"] = [REDIS_URL]
