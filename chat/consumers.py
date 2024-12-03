450b4699d5c7d1c57dcb13a1ee5ec239acd3276f
dmurawski <dmurawski018@gmail.com>
2024-12-03 20:53:45 +0100

    fix asgi.py

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,34 +1,33 @@
 import json
+
 from channels.generic.websocket import AsyncWebsocketConsumer
 from django.utils import timezone
+
 from chat.models import Message
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
+        print(self.scope)
         self.user = self.scope["user"]
         self.id = self.scope["url_route"]["kwargs"]["course_id"]
         self.room_group_name = f"chat_{self.id}"
         # join room group
-        await self.channel_layer.group_add(
-            self.room_group_name,
-            self.channel_name,
-        )
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+        # accept connection
         await self.accept()
 
     async def disconnect(self, close_code):
         # leave room group
-        await self.channel_layer.group_discard(
-            self.room_group_name,
-            self.channel_name,
-        )
+        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
+    # persist message to database
     async def persist_message(self, message):
-        # send message to WebSocket
         await Message.objects.acreate(
             user=self.user, course_id=self.id, content=message
         )
 
+    # receive message from WebSocket
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
--- educa/asgi.py
+++ educa/asgi.py
@@ -1,13 +1,15 @@
 """
 ASGI config for educa project.
 
-It exposes the ASGI callable as a module-level variable named ``application``.
+It exposes the ASGI callable as a module-level variable named
+``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
 import os
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
@@ -23,7 +25,7 @@ application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
         "websocket": AllowedHostsOriginValidator(
-            URLRouter(websocket_urlpatterns),
+            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
         ),
     }
 )
